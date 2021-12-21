3346e92e8a71fb00298e64bb035e6f0c9ea2df03
Allan <allan.montes@gmail.com>
2021-12-20 19:13:29 -0600

    12/20.3

--- base_code/modeling.py
+++ base_code/modeling.py
@@ -12,9 +12,9 @@ def linear_regression(x_train,y_train,x_test,y_test):
     model_LR = LinearRegression()
     model_LR.fit(x_train, y_train) #aquí está ocurriendo el entrenamiento
     # imprimimos los coeficientes
-    #model_LR.coef_
+    coef = model_LR.coef_
     # imprimimos el intercepto
-    #model_LR.intercept_
+    intercept = model_LR.intercept_
     # score
     train_score_r2_LR = model_LR.score(x_train,y_train)
     test_score_r2_LR = model_LR.score(x_test,y_test)
@@ -24,8 +24,7 @@ def linear_regression(x_train,y_train,x_test,y_test):
     # predicciones sobre el conjunto de validacion/test
     y_pred_test = model_LR.predict(x_test)
     rmse_test = sqrt(mean_squared_error(y_test,y_pred_test))
-
-    return train_score_r2_LR,test_score_r2_LR,rmse_train,rmse_test
+    return train_score_r2_LR,test_score_r2_LR,rmse_train,rmse_test,coef,intercept,model_LR
 
 
 def random_forest_regressor(x_train,y_train,x_test,y_test):
@@ -52,10 +51,12 @@ def manual_model(x_train,y_train,x_test,y_test):
     df_manual_model = pd.DataFrame()
     cols = str(list(x_train.columns))
     x_norm = x_train#StandardScaler().fit_transform(x_train)
+    x_norm = x_train#StandardScaler().fit_transform(x_train)
+    x_norm_tes = StandardScaler().fit_transform(x_test)
     # Build Linear Rgression
     df_manual_model.loc[0,"Model"] = "Linear Regression"
     df_manual_model.loc[0,"Columns"] = cols
-    train_score_r2_LR,test_score_r2_LR,rmse_train,rmse_test = linear_regression(x_norm,y_train,x_test,y_test)
+    train_score_r2_LR,test_score_r2_LR,rmse_train,rmse_test,coef,intercept,model_LR = linear_regression(x_norm,y_train,x_norm_tes,y_test)
     df_manual_model.loc[0,"r2 Train"] = train_score_r2_LR
     df_manual_model.loc[0,"r2 Validate"] = test_score_r2_LR
     df_manual_model.loc[0,"RMSE Train"] = rmse_train
@@ -68,7 +69,7 @@ def manual_model(x_train,y_train,x_test,y_test):
     df_manual_model.loc[1,"r2 Validate"] = test_score_r2_RFR
     df_manual_model.loc[1,"RMSE Train"] = rmse_train
     df_manual_model.loc[1,"RMSE Validate"] = rmse_test
-    return df_manual_model
+    return df_manual_model,coef,intercept,model_LR
 
 def final_model(x_train,y_train,x_test,y_test):
     import pandas as pd
